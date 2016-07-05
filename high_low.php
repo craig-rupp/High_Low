d4b87627f025cf595018d54e971639dda92ec74b
Craig Rupp <craigrupp@utexas.edu>
2016-07-05 09:46:52 -0500

    Add High & Low Game

---
+++ high_low.php
@@ -0,0 +1,20 @@
+<?php
+
+$randomNumber = rand(1,100);
+$guessCount = 0;
+
+do {
+	fwrite(STDOUT, 'Guess: ');
+	$guess = trim(fgets(STDIN));
+
+	if($guess < $randomNumber){
+		fwrite(STDOUT, "HIGHER\n")
+	} elseif ($guess > $randomNumber){
+		fwrite(STDOUT, "LOWER\n");
+	} else {
+		fwrite(STDOUT, "GOOD GUESS!!\n");
+	}
+	$guessCount++;
+} while ($guess != $randomNumber);
+
+fwrite(STDOUT, "You guessed $guessCount times.");
