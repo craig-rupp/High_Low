21876453d0cf06484fe70bb0705f94c9c5ff3043
Craig Rupp <craigrupp@utexas.edu>
2016-07-05 11:10:43 -0500

    User input alteration to High-Low

--- high_low.php
+++ high_low.php
@@ -1,14 +1,29 @@
 <?php
 
-$randomNumber = mt_rand(1,100);
+$min = 1;
+$max = 100;
+if ($argc >= 3){
+	if (is_numeric($argv[1]) && is_numeric($argv[2]) && $argv[1] < $argv[2]) {
+	$min = $argv[1];
+	$max = $argv[2];
+	fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]} see if you guessed correctly\n");
+	} else {
+	fwrite(STDERR, "You need to pass 2 numeric values\n");
+	exit(1);
+	}
+}
+
+$randomNumber = mt_rand($min, $max);
 $guessCount = 0;
+
+
 
 do {
 	fwrite(STDOUT, 'Guess: ');
 	$guess = trim(fgets(STDIN));
 
 	if($guess < $randomNumber){
-		fwrite(STDOUT, "HIGHER\n")
+		fwrite(STDOUT, "HIGHER\n");
 	} elseif ($guess > $randomNumber){
 		fwrite(STDOUT, "LOWER\n");
 	} else {
@@ -17,4 +32,4 @@ do {
 	$guessCount++;
 } while ($guess != $randomNumber);
 
-fwrite(STDOUT, "You guessed $guessCount times.");
+fwrite(STDOUT, "You guessed $guessCount times.\n");
